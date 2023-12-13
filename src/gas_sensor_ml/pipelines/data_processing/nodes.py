691302917e01bdcf76d44e7c86849f78dc841fd0
gavinlouuu <gavinlou@kingsphase.page>
2023-12-13 19:00:00 +0800

    combine feature

--- src/gas_sensor_ml/pipelines/data_processing/nodes.py
+++ src/gas_sensor_ml/pipelines/data_processing/nodes.py
@@ -19,6 +19,9 @@ def _hi_lo_peak(x: pd.DataFrame) -> pd.DataFrame:
     return smaller_peaks
 
 def data_stack(sp: pd.DataFrame, df: pd.DataFrame) -> pd.DataFrame:
+    """
+    After finding the peaks, stack the data according to exp_no
+    """
     df_stacked_list = []
     for i in range(len(sp) - 1):
         df_subset = df.iloc[sp[i]:sp[i + 1]].copy()
@@ -28,15 +31,11 @@ def data_stack(sp: pd.DataFrame, df: pd.DataFrame) -> pd.DataFrame:
         df_stacked = pd.concat(df_stacked_list, ignore_index=True)
     return df_stacked
 
-# def _group_by_bucket(df_stacked: pd.DataFrame, bucket_size_ms: int) -> pd.DataFrame:
-#     df_list = []
-#     grouped = df_stacked.groupby('exp_no')
-#     for name, group in grouped:
-#         group['timestamp_bucket'] = group['timestamp'].floordiv(bucket_size_ms)
-#         df_list.append(group)
-#     return pd.concat(df_list)
 
 def _group_by_bin(df_stacked: pd.DataFrame, num_bins: int) -> pd.DataFrame:
+    """
+    Use PD.CUT to group data into specified bins in parameters
+    """
     df_list = []
     grouped = df_stacked.groupby('exp_no')
     for name, group in grouped:
@@ -45,11 +44,19 @@ def _group_by_bin(df_stacked: pd.DataFrame, num_bins: int) -> pd.DataFrame:
     return pd.concat(df_list)
 
 def _average_bin(bin_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    average values within each bin to return only one data point
+    """
     bin_df = bin_df.drop(columns=['timestamp'])
     grouped = bin_df.groupby(['exp_no', 'bin']).mean()
     return grouped.reset_index()
 
 def preprocess_data_bin(mox: pd.DataFrame, num_bins: int) -> pd.DataFrame:
+    """
+    Return data that is sorted by experiment number according to lo_peak interval
+    data is stacked and labeled by exp_no
+    data is grouped by bin and averaged
+    """
     df_stacked = data_stack(_hi_lo_peak(mox), mox)
     bin_df = _group_by_bin(df_stacked, num_bins)
     mean_bin = _average_bin(bin_df)
@@ -86,11 +93,24 @@ def _transpose_(df_set: pd.DataFrame) -> pd.DataFrame:
     transposed.reset_index(inplace=True)
     return transposed
 
-def _remove_exp_no(df: pd.DataFrame) -> pd.DataFrame:
-    df = df.drop(columns=['exp_no'])
-    return df
+
+def _res_ratio(averaged: pd.DataFrame) -> pd.DataFrame:
+    def calculate_res_ratio(group):
+        return group['A1_Resistance'].max() / group['A1_Resistance'].min()
+
+    res_ratio = averaged.groupby('exp_no').apply(calculate_res_ratio).reset_index()
+    res_ratio.columns = ['exp_no', 'res_ratio']
+    return res_ratio
+
+def _combine_feature_matrix(res_ratio: pd.DataFrame, transposed: pd.DataFrame) -> pd.DataFrame:
+    combined = pd.merge(res_ratio, transposed, on='exp_no')
+    return combined
 
 def create_model_input_table(mox_bin: pd.DataFrame, percentile_bins: float) -> pd.DataFrame:
     selected_range = _group_percentile(mox_bin, percentile_bins)
-    mox_table = _remove_exp_no(_transpose_(selected_range))
+    # the ratio is from the entire dataset not filtered to be ground truth
+    res_ratio = _res_ratio(mox_bin) 
+    transpose_col = _transpose_(selected_range)
+    # drop exp_no to avoid training on exp_no
+    mox_table = _combine_feature_matrix(transpose_col, res_ratio).drop(columns=['exp_no'])
     return mox_table
